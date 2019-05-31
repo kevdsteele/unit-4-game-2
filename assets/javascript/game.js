1ea49961e5488382e03bf148cb71c1571cda4343
Kevin Steele <kevdsteele@gmail.com>
2019-05-31 11:50:40 -0400

    js to enable/disable attack btn

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,38 +41,63 @@ var characters = {
 
 }
 
-for (x in characters) {
-    var charBtn = $("<button>");
-    var btnTxt = characters[x].name + "<br>" +"<br>" + "<br>" + "<br>"+ "<br>" + "<br>" + "<br>" + "<br>" + "<br>" + "HP:" + characters[x].HP + "&nbsp" + "AP:" + characters[x].AP +  "&nbsp"+"DP:" + characters[x].DP;
-    charBtn.addClass("charBtn");
-    charBtn.attr("HP", characters[x].HP);
-    charBtn.attr("AP", characters[x].AP);
-    charBtn.attr("DP", characters[x].DP);
-    charBtn.html(btnTxt);
-    charBtn.css({"background-image" : "url(" + characters[x].image + ")", "bacgroundRepeat": "no-repeat"});
-    $("#playerChoices").append(charBtn);
+$("#attackBtn").attr("disabled", true);
+
+function playGame(){
+
+    $(".charBtn").on("click", function() {
+        console.log("Clicked");
+        if (isPlayerChosen){
+            $(this).clone().appendTo("#defenderPlaceholder");
+             defenderHP=parseInt($(this).attr("HP"));
+             defenderDP=parseInt($(this).attr("DP"));
+             console.log("Defender HP: " + defenderHP);
+             console.log("Defender DP: " + defenderDP);
+            $(this).remove();
+           
+            battle();
+            
+        }
+        else {
+            $(this).clone().appendTo("#playerPlaceholder"); 
+            playerHP=parseInt($(this).attr("HP"));
+            playerDP=parseInt($(this).attr("DP"));
+            console.log("Player HP: " + playerHP);
+             console.log("Player DP: " + playerDP);
+            $(this).remove();
+            
+        }
+        isPlayerChosen=true;
+        
+        });    
 }
-$(".charBtn").on("click", function() {
-console.log("Clicked");
-if (isPlayerChosen){
-    $(this).clone().appendTo("#defenderPlaceholder");
-     defenderHP=parseInt($(this).attr("HP"));
-     defenderDP=parseInt($(this).attr("DP"));
-     console.log("Defender HP: " + defenderHP);
-     console.log("Defender DP: " + defenderDP);
-    $(this).remove();
-    
+
+function initializeChars () {
+    for (x in characters) {
+        var charBtn = $("<button>");
+        var btnTxt = characters[x].name + "<br>" +"<br>" + "<br>" + "<br>"+ "<br>" + "<br>" + "<br>" + "<br>" + "<br>" + "HP:" + characters[x].HP + "&nbsp" + "AP:" + characters[x].AP +  "&nbsp"+"DP:" + characters[x].DP;
+        charBtn.addClass("charBtn");
+        charBtn.attr("HP", characters[x].HP);
+        charBtn.attr("AP", characters[x].AP);
+        charBtn.attr("DP", characters[x].DP);
+        charBtn.html(btnTxt);
+        charBtn.css({"background-image" : "url(" + characters[x].image + ")", "bacgroundRepeat": "no-repeat"});
+        $("#playerChoices").append(charBtn);
+    }
+
 }
-else {
-    $(this).clone().appendTo("#playerPlaceholder"); 
-    playerHP=parseInt($(this).attr("HP"));
-    playerDP=parseInt($(this).attr("DP"));
-    console.log("Player HP: " + playerHP);
-     console.log("Player DP: " + playerDP);
-    $(this).remove();
+
+function battle(){
+    $("#attackBtn").attr("disabled", false);
+    $("#attackBtn").on("click", function() {
+        
+
+    });
 }
-isPlayerChosen=true;
-});
+
+
+initializeChars();
+playGame();
 
 
 
