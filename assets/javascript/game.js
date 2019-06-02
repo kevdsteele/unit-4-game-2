1fceec421afc10e64075d0aac934e2eb0be9e89b
Kevin Steele <kevdsteele@gmail.com>
2019-06-01 20:38:52 -0400

    Tweaking game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+  
+
+var defenderID ="";
+var playerID =""; 
+var playerStartHP=0;
+var defenderStartHP=0;
+var numberCharacters=0;
 
 var isPlayerChosen=false;
 var playerName="";
@@ -10,7 +17,7 @@ var defenderHP=0;
 var defenderDP=0;
 var round=1;
 var attackMod=0;
-var btnTxt="";
+
 var characters = {
     "player1" : {
         "name": "Luke Skywalker",
@@ -24,21 +31,21 @@ var characters = {
         "name": "Yoda",
         "HP": 200,
         "AP": 25,
-        "DP": 12,
+        "DP": 20,
         "image": "assets/images/yoda.png"
     },
     "player3" : {
         "name": "Darth Vader",
         "HP": 150,
-        "AP": 15,
-        "DP": 10,
+        "AP": 25,
+        "DP": 15,
         "image": "assets/images/dv.jpg"
     },
     "player4" : {
         "name": "Darth Sidiuus",
         "HP": 175,
         "AP": 25,
-        "DP": 15,
+        "DP": 25,
         "image": "assets/images/ds.jpg"
     }
 
@@ -55,11 +62,12 @@ function playGame(){
             $("#defenderPlaceholder").empty();
             console.log("round:"+ round);
             defenderHP=parseInt($(this).attr("HP"));
+            defenderStartHP=defenderHP;
             defenderDP=parseInt($(this).attr("DP"));
-            playerName=$(this).attr("name");
-            btnTxt=playerName;
-            $("#defenderStats").html("HP:" + defenderHP + "&nbsp"+ "DP:" + defenderDP);
-            $(this).html(btnTxt);
+            defenderName=$(this).attr("name");
+            defenderID=$(this).attr("charid");
+            
+            
            
            
             $(this).clone().appendTo("#defenderPlaceholder");
@@ -77,11 +85,12 @@ function playGame(){
 
         } else if (isPlayerChosen){
              defenderHP=parseInt($(this).attr("HP"));
+             defenderStartHP=defenderHP;
              defenderDP=parseInt($(this).attr("DP"));
-             playerName=$(this).attr("name");
-             btnTxt=playerName;
-             $(this).html(btnTxt);
-             $("#defenderStats").html("HP:" + defenderHP + "&nbsp"+"DP:" + defenderDP);
+             defenderName=$(this).attr("name");
+             defenderID=$(this).attr("charid");
+             
+             
              $(this).clone().appendTo("#defenderPlaceholder");
              console.log("Defender HP: " + defenderHP);
              console.log("Defender DP: " + defenderDP);
@@ -93,18 +102,18 @@ function playGame(){
         else {
            
             playerHP=parseInt($(this).attr("HP"));
+            playerStartHP=playerHP;
             playerAP=parseInt($(this).attr("AP"));
             playerName=$(this).attr("name");
-            btnTxt=playerName; 
+            playerID=$(this).attr("charid");
             attackMod=playerAP;
-            /*$(".stats").html("HP:" + playerHP + "&nbsp"+ "AP:" + playerAP);*/
-            $("#playerStats").html("HP:" + playerHP + "&nbsp"+ "AP:" + playerAP);
+           
             $(this).clone().appendTo("#playerPlaceholder"); 
             console.log("Player HP: " + playerHP);
              console.log("Player DP: " + playerAP);
             $(this).remove();
             isPlayerChosen=true;
-            
+            $("#playerHeading").html("Choose an Enemy");
         }
         
         
@@ -113,12 +122,15 @@ function playGame(){
 
 function initializeChars () {
     for (x in characters) {
+        numberCharacters++;
         var charDiv1 = $("<div>");
         charDiv1.addClass("row charBox");
         charDiv1.attr("id", "Row1"+x);
         charDiv1.attr("HP", characters[x].HP);
         charDiv1.attr("AP", characters[x].AP);
         charDiv1.attr("DP", characters[x].DP);
+        charDiv1.attr("charID", x);
+        charDiv1.attr("name",characters[x].name);
         var charDiv2= $("<div>");
         
         charDiv2.attr("id", "Row2"+x);
@@ -128,17 +140,29 @@ function initializeChars () {
         charDiv3.attr("id", "Row3"+x);
         charDiv3.html("<img src='" + characters[x].image + "'>");
         var charDiv4 = $("<div>");
-        charDiv4.addClass("stats", "HP:" + playerHP + "&nbsp"+ "AP:" + playerAP);
+        charDiv4.addClass("progress");
+       
         charDiv4.attr("id", "Row4"+x);
+
+        var charDiv5=$("<div>");
+        charDiv5.addClass("progress-bar")
+        charDiv5.attr("id", x);
+        charDiv5.attr("role", "progressbar");
+        charDiv5.attr("aria-valuenow",characters[x].HP );
+        charDiv5.attr("aria-valuemax",characters[x].HP );
+        charDiv5.attr("aria-valuemin",0);
+        charDiv5.html(characters[x].HP +" HPs");
+        charDiv5.attr("style" , "width: 100%");
+
         
-        charDiv4.html("HP: " + characters[x].HP  + "&nbsp"+ "AP: " + characters[x].AP + "&nbsp" + "DP: " +characters[x].DP );
-        $("#game-characters").append(charDiv1);
+       
+       
         $("#playerChoices").append(charDiv1);
         $("#Row1"+x).append(charDiv2);
         $("#Row2"+x).append(charDiv3);
         $("#Row3"+x).append(charDiv4);
-
-       /* btnTxt = characters[x].name + "<br>" +"<br>" + "<br>" + "<br>"+ "<br>" + "<br>" + "<br>" + "<br>" + "<br>" + "HP:" + characters[x].HP + "&nbsp" + "AP:" + characters[x].AP +  "&nbsp"+"DP:" + characters[x].DP;
+        $("#Row4"+x).append(charDiv5);
+       /*btnTxt = characters[x].name + "<br>" +"<br>" + "<br>" + "<br>"+ "<br>" + "<br>" + "<br>" + "<br>" + "<br>" + "HP:" + characters[x].HP + "&nbsp" + "AP:" + characters[x].AP +  "&nbsp"+"DP:" + characters[x].DP;
         charBtn.addClass("charBtn btn btn-block");
         charBtn.attr("name", characters[x].name);
         charBtn.attr("HP", characters[x].HP);
@@ -163,31 +187,51 @@ function battle(){
         console.log("player AP:"+ playerAP);
         if (playerHP && defenderHP > 0) {
             defenderHP-=playerAP;
+            
+            $("#"+defenderID).attr("aria-valuenow", defenderHP);
+            $("#"+defenderID).html(defenderHP);
+            $("#"+defenderID).attr("style" , "width: " +  (defenderHP * 100) /defenderStartHP + "%");
+            $("#defenderStats").html(playerName + " did "+ playerAP + " points of damage to " + defenderName);
             playerAP+=attackMod;
-            $("#defenderStats").html("HP:" + defenderHP + "&nbsp"+ "DP:" + defenderDP);
+           
             console.log("Defender hp:" + defenderHP);
             if (defenderHP >0) {
                 playerHP-=defenderDP;
-                $("#playerStats").html("HP:" + playerHP + "&nbsp"+ "AP:" + playerAP);
+                $("#"+playerID).html(playerHP);
+                $("#"+playerID).attr("style" , "width: " +  (playerHP * 100) / playerStartHP + "%");
+                $("#playerStats").html(playerName + " took " + defenderDP + " points of damage from " + defenderName);
+               
                 console.log("Player HP:" + playerHP);
                 if (playerHP <=0) {
                     console.log ("You Lose");
+                    $("#attack-img").fadeTo("slow" , 0, function() {
+                    
+                    });
                     $("#attack-text").text("You Lost");
                 }
             } else {
                 console.log("You Win");
-                $("#attack-img").fadeTo("slow" , 0.25, function() {
+                $("#attack-img").fadeTo("slow" , 0, function() {
                     
                   });
+
+                if (round < (Object.keys(characters).length -1)) {
+                    
                 $("#attack-text").text("You Won Round "+ round +"! " + "Choose another enemy");
                 $("#defenderPlaceholder").clone().appendTo("#defenderLosers");
                 $("#defenderStats").html("");
+                $("#playererStats").html("");
                 $("#defenderPlaceholder").empty();
                 $(".charBox").css("pointer-events", "auto");
                 round++;
                 $("#attackBtn").css("pointer-events", "none");
                 playGame();
-                
+                } else {
+
+                    
+                }
+
+            
                 
 
             
