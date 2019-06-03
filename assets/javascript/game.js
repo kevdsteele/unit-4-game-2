32f44e9a2f1b6cfb2d36122d88d888083146bb78
kevdsteele <48927209+kevdsteele@users.noreply.github.com>
2019-06-03 17:03:07 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -61,7 +61,7 @@ function playGame(){
     $("#attack-img").hide();
   
     $(".card").on("mouseover", function() {
-  $(this).classList.toggle("is-flipped");
+  $(this).toggleClass("is-flipped");
 });
 
     $(".charBox").on("click", function() {
